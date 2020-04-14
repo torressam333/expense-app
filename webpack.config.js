ef0f21175f8318354229ccacbd87a2bc77df0999
Samuel Torres <torressam333@gmail.com>
2020-04-14 07:18:50 -0700

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+//Entry point of our app
+module.exports = {
+  //config details for webpack build
+    entry: './src/app.js',
+    //Where to output bundle.js
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        },{
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+    }
+};

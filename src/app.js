a4011d3452e16673da2d0ab2f9f62068dfc10df5
Samuel Torres <torressam333@gmail.com>
2020-04-16 06:16:28 -0700

    Bring in react-router-dom for linking pages

--- src/app.js
+++ src/app.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import "normalize.css/normalize.css";
+import './styles/styles.scss';
 
-import "normalize.css/normalize.css"
-import './styles/styles.scss'
+const ExpenseDashboardPage = () =>(
+  <div>This is from my dashboard component</div>
+);
 
+const AddExpensePage = () =>(
+    <div>This is from my Add Expense component</div>
+);
 
-ReactDOM.render(<p>Start of Expense App</p>, document.getElementById('app'));
+const EditExpensePage = () => (
+  <div>This is the edit page</div>
+);
+
+const HelpPage = () => (
+    <div>This is the help page</div>
+);
+
+const NotFound = () => (
+    <div>404 - Page Not Found</div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage}/>
+      <Route path="/help" component={HelpPage}/>
+      <Route component={NotFound}/>
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));

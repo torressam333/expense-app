1890b257a2d649dcb97b05ca2cf49f26ca365d36
Samuel Torres <torressam333@gmail.com>
2020-04-29 06:26:48 -0700

    Practice with changing state dynamically in Redux Store using Actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,52 @@
 import {createStore} from "redux";
 
-const store = createStore((state = {count:0}) => {
-    return state;
+const store = createStore((state = {count:0}, action) => {
+    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+    const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy: 1;
+
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + incrementBy
+            }
+        case 'DECREMENT':
+            return {
+                count: state.count - decrementBy
+            }
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default:
+            return state;
+    }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() =>{
+    console.log(store.getState());
+});
+
+
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 100
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+//Reset the count
+store.dispatch({
+    type: 'RESET'
+});
+
+//Decrement
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 15
+});

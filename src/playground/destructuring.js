afc3a083d050f3acf4501bf76874c8a24b3276b6
Samuel Torres <torressam333@gmail.com>
2020-05-11 08:26:30 -0700

    Workout array destructuring example

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+//OBJECT DESTRUCTURING
+
 /*
 const person ={
     name: 'Sam',
@@ -16,7 +18,6 @@ const {city, temp: temperature} = person.location;
 if (city && temperature) {
     console.log(`It is ${temperature} in ${city}`);
 }
-*/
 
 const book = {
     title: 'Ego is the Enemy',
@@ -29,6 +30,18 @@ const book = {
 const {name: publisherName = 'Self-Published'} = book.publisher;
 
     console.log(publisherName);
+*/
 
 
 
+//ARRAY DESTRUCTURING
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19149'];
+const [, city, state = 'Arizona'] = address;
+
+console.log(`You are in ${city} ${state}`);
+
+const item = ['Espresso', '$2.00', '$2.50', '$2.75'];
+
+const [coffee, , doubleShot] = item;
+
+console.log(`A double shot of ${coffee} costs ${doubleShot}`);
